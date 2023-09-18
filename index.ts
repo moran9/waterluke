9564166c50f881c0383787bd26d04a852aad3afd
Cristina Morán <cristina.moran.capa@gmail.com>
2023-09-18 22:09:41 +0200

    include eslint and prettier with basic config

--- index.ts
+++ index.ts
@@ -1,45 +1,54 @@
-import { fastify, FastifyInstance } from 'fastify'
+import { fastify, FastifyInstance } from 'fastify';
 
-const server: FastifyInstance = fastify()
+const server: FastifyInstance = fastify();
 
-interface IQuerystring {
+type IQuerystring = {
     username: string;
     password: string;
-  }
-  
-  interface IHeaders {
+};
+
+type IHeaders = {
     'h-Custom': string;
-  }
-  
-  type IReply = {
+};
+
+type BasicReply = {
     200: { success: boolean };
     302: { url: string };
     '4xx': { error: string };
-  }| string
+};
 
+type IReply = BasicReply | string;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+    return 'pong\n';
+});
 
 server.get<{
-    Querystring: IQuerystring,
-    Headers: IHeaders,
-    Reply: IReply
-  }>('/auth', {
-    preValidation: (request, reply, done) => {
-      const { username, password } = request.query
-      done(username !== 'admin' ? new Error('Must be admin') : undefined) // only validate `admin` account
-    }
-  }, async (request, reply) => {
-    const customerHeader = request.headers['h-Custom']
-    // do something with request data
-    return `logged in!`
-  })
+    Querystring: IQuerystring;
+    Headers: IHeaders;
+    Reply: IReply;
+}>(
+    '/auth',
+    {
+        preValidation: (request, reply, done) => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const { username, password } = request.query;
+            done(username !== 'admin' ? new Error('Must be admin') : undefined); // only validate `admin` account
+        },
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async (request, reply) => {
+        //const customerHeader = request.headers['h-Custom'];
+        // do something with request data
+        return `logged in!`;
+    },
+);
 
 server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-})
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
