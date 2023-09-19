5da9976cd5ac9744e2bd3dc7bec6477f362caad0
Cristina Morán <cristina.moran.capa@gmail.com>
2023-09-19 16:08:56 +0200

    extract server from index.ts

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import { fastify, FastifyInstance } from 'fastify';
+
+export const app: FastifyInstance = fastify({ logger: true });
+
+type IQuerystring = {
+    username: string;
+    password: string;
+};
+
+type IHeaders = {
+    'h-Custom': string;
+};
+
+type BasicReply = {
+    200: { success: boolean };
+    302: { url: string };
+    '4xx': { error: string };
+};
+
+type IReply = BasicReply | string;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.get('/ping', async (request, reply) => {
+    return 'pong\n';
+});
+
+app.get<{
+    Querystring: IQuerystring;
+    Headers: IHeaders;
+    Reply: IReply;
+}>(
+    '/auth',
+    {
+        preValidation: (request, reply, done) => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const { username, password } = request.query;
+            done(username !== 'admin' ? new Error('Must be admin') : undefined); // only validate `admin` account
+        },
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async (request, reply) => {
+        //const customerHeader = request.headers['h-Custom'];
+        // do something with request data
+        return `logged in!`;
+    },
+);
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,15 @@
-import { fastify, FastifyInstance } from 'fastify';
+import { app } from './app';
 import { openSerialPort, closeSerialPort } from './serialPort';
 
-const server: FastifyInstance = fastify();
+// Connect to serial port
+openSerialPort();
+setTimeout(closeSerialPort, 5000);
 
-type IQuerystring = {
-    username: string;
-    password: string;
-};
-
-type IHeaders = {
-    'h-Custom': string;
-};
-
-type BasicReply = {
-    200: { success: boolean };
-    302: { url: string };
-    '4xx': { error: string };
-};
-
-type IReply = BasicReply | string;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-server.get('/ping', async (request, reply) => {
-    return 'pong\n';
-});
-
-server.get<{
-    Querystring: IQuerystring;
-    Headers: IHeaders;
-    Reply: IReply;
-}>(
-    '/auth',
-    {
-        preValidation: (request, reply, done) => {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            const { username, password } = request.query;
-            done(username !== 'admin' ? new Error('Must be admin') : undefined); // only validate `admin` account
-        },
-    },
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    async (request, reply) => {
-        //const customerHeader = request.headers['h-Custom'];
-        // do something with request data
-        return `logged in!`;
-    },
-);
-
-server.listen({ port: 8080 }, (err, address) => {
+// Start server
+app.listen({ port: 8080 }, (err, address) => {
     if (err) {
         console.error(err);
         process.exit(1);
     }
     console.log(`Server listening at ${address}`);
-
-    // Stablish connection with serial port
-    openSerialPort();
-    setTimeout(closeSerialPort, 5000);
 });
