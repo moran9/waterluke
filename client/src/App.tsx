d3a0cc4e8284b0e257bb14a20587262d218c29cd
Cristina Morán <cristina.moran.capa@gmail.com>
2023-09-23 19:04:05 +0200

    add redux toolkit example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Counter } from './features/counter/Counter'
 
 function App() {
     const [count, setCount] = useState(0)
@@ -16,6 +17,8 @@ function App() {
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
+            <h1>redux toolkit</h1>
+            <Counter />
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
---
+++ client/src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+    },
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
+
+export default store
